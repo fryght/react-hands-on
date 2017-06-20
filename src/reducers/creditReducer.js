973122f62c19cbacf6724b7650c16dc35e851c19
Mark Brouwers <mbrouwers@computest.nl>
2017-06-20 16:18:30 +0200

    Small tweak

--- src/reducers/creditReducer.js
+++ src/reducers/creditReducer.js
@@ -3,8 +3,7 @@ import credit from '../data/credit';
 const creditReducer = (state = credit, action) => {
     switch (action.type) {
         case 'ADD_PAYMENT':
-            state = state.set(action.id, [...state.get(action.id), action.payment]);
-            return state;
+            return state.set(action.id, [...state.get(action.id), action.payment]);
         default:
             return state;
     }
